8ca9bb440456f16788e440e1fd9b6d936b4e5390
Tarun Vuppala <tarun.vuppala26@gmail.com>
2024-07-10 10:00:16 +0530

    env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const mongoose=require('mongoose')
+const dotenv=require('dotenv')
 const app=express()
 
+dotenv.config()
 app.get('/',(req,res)=>{
     res.send("Hello")
 })
