f44fa59ea19dbee79b4cb92b0af94321166e80c9
Tarun Vuppala <tarun.vuppala26@gmail.com>
2024-08-14 23:03:04 +0530

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import OtpInput from './auth/OTP'
+import { Routes, Route } from 'react-router-dom';
+import OtpInput from './auth/OTP';
+import Signup from './auth/Signup';
+import Login from './auth/Login';
+import ResetPassword from './auth/ResetPassword';
+
 
 const App = () => {
   return (
-    <div className='flex flex-col justify-center items-center h-screen'>
-      <OtpInput />
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<></>} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/otp' element={<OtpInput />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Routes>
+
+      <div className='flex flex-col justify-center items-center h-screen'>
+        Home
+      </div>
+    </>
   )
 }
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Navbar from './components/Navbar';
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NextUIProvider>
-      <div className="bg-primary min-h-screen px-[4rem]">
-        <Navbar />
-        <App />
-      </div>
+      <BrowserRouter>
+        <div className="bg-primary min-h-screen px-[4rem]">
+          <Navbar />
+          <App />
+        </div>
+      </BrowserRouter>
     </NextUIProvider>
   </React.StrictMode>
 );
