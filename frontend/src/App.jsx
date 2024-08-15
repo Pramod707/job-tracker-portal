1b0e385ff5ba98cdeca53272f25f8ef874665431
Tarun Vuppala <tarun.vuppala26@gmail.com>
2024-08-15 11:39:01 +0530

    merged conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-<<<<<<< Updated upstream
 import { Routes, Route } from 'react-router-dom';
 import OtpInput from './auth/OTP';
 import Signup from './auth/Signup';
 import Login from './auth/Login';
 import ResetPassword from './auth/ResetPassword';
-
+import Sidebar from './components/Sidebar'
+import Details from './auth/Details'
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<></>} />
+        <Route path='/' element={<Sidebar />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/add-details' element={<Details />} />
         <Route path='/login' element={<Login />} />
         <Route path='/otp' element={<OtpInput />} />
         <Route path='/reset-password' element={<ResetPassword />} />
@@ -22,17 +23,7 @@ const App = () => {
         Home
       </div>
     </>
-=======
-import Sidebar from './components/Sidebar'
-import Details from './auth/Details'
-
-const App = () => {
-  return (
-    <div className='h-screen'>
-      <Details />
-    </div>
->>>>>>> Stashed changes
   )
 }
 
-export default App
+export default App;
