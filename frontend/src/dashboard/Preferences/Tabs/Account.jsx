7577baa427e9dcf5d0a2684434a9486fbc204914
madhavmadupu <madhav.madupu@gmail.com>
2024-08-19 10:40:53 +0530

    Preferences/Account Done!

--- frontend/src/dashboard/Preferences/Tabs/Account.jsx
+++ frontend/src/dashboard/Preferences/Tabs/Account.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from '@nextui-org/react'
+import { Button, Input } from '@nextui-org/react'
 
 const Account = () => {
   return (
@@ -8,13 +8,18 @@ const Account = () => {
       <div className='flex flex-col gap-[1rem]'>
         <h2 className='font-black'>Profile Picture</h2>
         <div className='flex flex-row  justify-start items-center gap-[1rem]'>
-          <img src="/profile.png" alt="Profile" className='w-[4rem]' />
+          <img src="/profile.png" alt="Profile" className='w-[4rem] border-2 rounded-full p-[0.1rem]' />
           <Button className='bg-transparent border border-gray-600'>Change</Button>
           <Button className='bg-red-100 border border-red-400'>Remove</Button>
         </div>
-
-        
-
+      </div>
+      <div className='flex flex-col gap-[1rem]'>
+        <h2 className='font-black'>Your Name</h2>
+        <Input variant='bordered' size='lg' placeholder='John Doe' />
+      </div>
+      <div className='flex flex-col gap-[1rem]'>
+        <h2 className='font-black'>Email</h2>
+        <Input variant='bordered' size='lg' placeholder='john@abc.com' />
       </div>
     </div>
   )
