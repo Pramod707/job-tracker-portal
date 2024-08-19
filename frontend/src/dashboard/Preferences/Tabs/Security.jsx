c6224fdd1720600487c863cfb7b3fca87b36a8d5
madhavmadupu <madhav.madupu@gmail.com>
2024-08-19 10:54:51 +0530

    Preferences/Security Done!

--- frontend/src/dashboard/Preferences/Tabs/Security.jsx
+++ frontend/src/dashboard/Preferences/Tabs/Security.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Checkbox, Input, Button } from '@nextui-org/react';
 
 const Security = () => {
   return (
-    <div>
-      <h1 className='font-black text-2xl'>Security</h1>
+    <div className='flex flex-col justify-start gap-[2rem]'>
+      <h1 className='font-black text-2xl'>Notifications</h1>
+      <div className='flex flex-col gap-[2rem] border-b-2 border-dotted pb-[2rem]'>
+        <div className='flex flex-row justify-between items-center gap-[2rem]'>
+          <div className='w-full flex flex-col'>
+            <h2 className='font-black '>Password</h2>
+            <p className='text-[#72908D]'>Change the password for your account</p>
+          </div>
+          <Button className='px-[2rem] font-black shadow-sm' variant='bordered'>Change Password</Button>
+        </div>
+        <div className='flex flex-row justify-between items-center gap-[2rem]'>
+          <div className='w-full flex flex-col'>
+            <h2 className='font-black '>Two-Factor Authentication</h2>
+            <p className='text-[#72908D]'>Require authentication when you login</p>
+          </div>
+          <Button className='px-[2rem] font-black shadow-sm' variant='bordered'>Enable</Button>
+        </div>
+        <div className='flex flex-row justify-between items-center gap-[2rem]'>
+          <div className='w-full flex flex-col'>
+            <h2 className='font-black '>Security</h2>
+            <p className='text-[#72908D]'>Log out of all sessions except this current browser</p>
+          </div>
+          <Button className='px-[2rem] font-black shadow-sm' variant='bordered'>Log Out All Sessions</Button>
+        </div>
+      </div>
+      <div className='flex flex-row justify-between items-center'>
+        <div className='w-full flex flex-col'>
+          <h2 className='font-black text-red-500'>Delete Account</h2>
+          <p className='text-[#72908D] w-[80%]'>Permanently delete the account and remove access from all workspace</p>
+        </div>
+        <Button className='px-[2rem] font-black shadow-sm' variant='solid' color='danger'>Delete Account</Button>
+      </div>
     </div>
   )
 }
