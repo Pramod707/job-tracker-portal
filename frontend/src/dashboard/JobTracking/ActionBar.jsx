f3c274b5e43c5ccb348010c750192ffa5776b061
Tarun Vuppala <tarun.vuppala26@gmail.com>
2024-09-18 12:02:23 +0530

    Action Bar fix

--- frontend/src/dashboard/JobTracking/ActionBar.jsx
+++ frontend/src/dashboard/JobTracking/ActionBar.jsx
@@ -7,113 +7,117 @@ import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import PopUp from './PopUp';
-const ActionBar = () => {
-  return (
-    <div className='border-b-3 border-dotted pb-[1rem] flex flex-row justify-between items-center'>
-        {/* Views */}
-        <div className='flex flex-row justify-center items-center gap-[1rem]'>
-            <Button
-                className="bg-[#DCE3E3] font-black"
-            >Table
-            </Button>
-            
-            <Button
-                className="bg-[#F4F6F6] font-black"
-            >Kanban
-            </Button>
 
-            <Button
-                className="bg-[#F4F6F6] font-black"
-            >Calendar
-            </Button>            
-        </div>
+const ActionBar = ({ setView }) => {
+    return (
+        <div className='border-b-3 border-dotted pb-[1rem] flex flex-row justify-between items-center'>
+            {/* Views */}
+            <div className='flex flex-row justify-center items-center gap-[1rem]'>
+                <Button
+                    className="bg-[#DCE3E3] font-black"
+                    onClick={() => setView("Table")}
+                >Table
+                </Button>
+
+                <Button
+                    className="bg-[#F4F6F6] font-black"
+                    onClick={() => setView("Kanban")}
+                >Kanban
+                </Button>
+
+                <Button
+                    className="bg-[#F4F6F6] font-black"
+                // onClick={()=>setView("Kanban")}
+                >Calendar
+                </Button>
+            </div>
 
 
 
-        {/* Utilities */}
-        <div className='flex flex-row justify-center items-center gap-[1rem]'>
-            <Dropdown backdrop="blur">
-                <DropdownTrigger>
-                    <Button variant="faded" className=" border border-[#EDF1F1] bg-transparent">
-                    Column<span><ViewStreamOutlinedIcon className='text-[#4F7471]'/></span>
-                    </Button>
-                </DropdownTrigger>
-                <DropdownMenu 
-                    aria-label="Select Columns"
-                    closeOnSelect={false}
-                    disallowEmptySelection
-                    selectionMode="multiple"
-                >
-                    <DropdownItem key="opportunity">Opportunity Name</DropdownItem>
-                    <DropdownItem key="company">Company</DropdownItem>
-                    <DropdownItem key="added">Added By</DropdownItem>
-                    <DropdownItem key="due">Due date</DropdownItem>
-                    <DropdownItem key="status">Status</DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
+            {/* Utilities */}
+            <div className='flex flex-row justify-center items-center gap-[1rem]'>
+                <Dropdown backdrop="blur">
+                    <DropdownTrigger>
+                        <Button variant="faded" className=" border border-[#EDF1F1] bg-transparent">
+                            Column<span><ViewStreamOutlinedIcon className='text-[#4F7471]' /></span>
+                        </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu
+                        aria-label="Select Columns"
+                        closeOnSelect={false}
+                        disallowEmptySelection
+                        selectionMode="multiple"
+                    >
+                        <DropdownItem key="opportunity">Opportunity Name</DropdownItem>
+                        <DropdownItem key="company">Company</DropdownItem>
+                        <DropdownItem key="added">Added By</DropdownItem>
+                        <DropdownItem key="due">Due date</DropdownItem>
+                        <DropdownItem key="status">Status</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
 
-            <Dropdown backdrop="blur">
-                <DropdownTrigger>
-                    <Button variant="faded" className=" border border-[#EDF1F1] bg-transparent">
-                    Filter<span><ExpandMoreOutlinedIcon className='text-[#4F7471]'/></span>
-                    </Button>
-                </DropdownTrigger>
-                <DropdownMenu 
-                    aria-label="Change View"
-                    closeOnSelect={false}
-                    disallowEmptySelection
-                    selectionMode="multiple"
-                >
-                    <DropdownItem key="opportunity">Opportunity</DropdownItem>
-                    <DropdownItem key="company">Company</DropdownItem>
-                    <DropdownItem key="added">Added By</DropdownItem>
-                    <DropdownItem key="due">Due date</DropdownItem>
-                    <DropdownItem key="status">Status</DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
+                <Dropdown backdrop="blur">
+                    <DropdownTrigger>
+                        <Button variant="faded" className=" border border-[#EDF1F1] bg-transparent">
+                            Filter<span><ExpandMoreOutlinedIcon className='text-[#4F7471]' /></span>
+                        </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu
+                        aria-label="Change View"
+                        closeOnSelect={false}
+                        disallowEmptySelection
+                        selectionMode="multiple"
+                    >
+                        <DropdownItem key="opportunity">Opportunity</DropdownItem>
+                        <DropdownItem key="company">Company</DropdownItem>
+                        <DropdownItem key="added">Added By</DropdownItem>
+                        <DropdownItem key="due">Due date</DropdownItem>
+                        <DropdownItem key="status">Status</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
 
-            <Dropdown backdrop="blur">
-                <DropdownTrigger>
-                    <Button variant="faded" className=" border border-[#EDF1F1] bg-transparent">
-                    Nearest Due Date<span><CalendarMonthOutlinedIcon className='text-[#4F7471]'/></span>
-                    </Button>
-                </DropdownTrigger>
-                <DropdownMenu 
-                    aria-label="Change View"
-                    closeOnSelect={false}
-                    disallowEmptySelection
-                    selectionMode="multiple"
-                >
-                    <DropdownItem key="opportunity">Opportunity</DropdownItem>
-                    <DropdownItem key="company">Company</DropdownItem>
-                    <DropdownItem key="added">Added By</DropdownItem>
-                    <DropdownItem key="due">Due date</DropdownItem>
-                    <DropdownItem key="status">Status</DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
+                <Dropdown backdrop="blur">
+                    <DropdownTrigger>
+                        <Button variant="faded" className=" border border-[#EDF1F1] bg-transparent">
+                            Nearest Due Date<span><CalendarMonthOutlinedIcon className='text-[#4F7471]' /></span>
+                        </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu
+                        aria-label="Change View"
+                        closeOnSelect={false}
+                        disallowEmptySelection
+                        selectionMode="multiple"
+                    >
+                        <DropdownItem key="opportunity">Opportunity</DropdownItem>
+                        <DropdownItem key="company">Company</DropdownItem>
+                        <DropdownItem key="added">Added By</DropdownItem>
+                        <DropdownItem key="due">Due date</DropdownItem>
+                        <DropdownItem key="status">Status</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
 
-            {/* <Button className="bg-[#0037FF] text-white font-black">
+                {/* <Button className="bg-[#0037FF] text-white font-black">
                 New <span><AddOutlinedIcon /></span> */}
-                <PopUp/>
-            {/* </Button> */}
+                <PopUp />
+                {/* </Button> */}
 
-            <Dropdown backdrop="blur">
-                <DropdownTrigger className="flex border  rounded-md cursor-pointer">
-                        <MoreVertOutlinedIcon className='text-[#4F7471]'/>
-                </DropdownTrigger>
-                <DropdownMenu 
-                    aria-label="Change View"
-                    closeOnSelect={false}
-                    disallowEmptySelection
-                    selectionMode="single"
-                >
-                    <DropdownItem key="opportunity">Clear All</DropdownItem>
-                    <DropdownItem key="company">Bulk Edit</DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
+                <Dropdown backdrop="blur">
+                    <DropdownTrigger className="flex border  rounded-md cursor-pointer">
+                        <MoreVertOutlinedIcon className='text-[#4F7471]' />
+                    </DropdownTrigger>
+                    <DropdownMenu
+                        aria-label="Change View"
+                        closeOnSelect={false}
+                        disallowEmptySelection
+                        selectionMode="single"
+                    >
+                        <DropdownItem key="opportunity">Clear All</DropdownItem>
+                        <DropdownItem key="company">Bulk Edit</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ActionBar
--- frontend/src/dashboard/JobTracking/JobTracking.jsx
+++ frontend/src/dashboard/JobTracking/JobTracking.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ActionBar from "./ActionBar"
 import JobsTable from "./JobsTable"
-
+import Kanban from "./Kanban"
 const JobTracking = () => {
+  const [view, setView] = useState('Table');
   return (
     <div>
-      <ActionBar />
+      <ActionBar setView={setView} />
       <div className='py-[1rem]'>
-        <JobsTable />
+        {view === 'Table' && <JobsTable />}
+        {view === 'Kanban' && <Kanban />}
       </div>
     </div>
   )
